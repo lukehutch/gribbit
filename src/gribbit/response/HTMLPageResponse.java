0030d4fbe6e5906a8b2be1d81c1694b3f3e8bad1
Luke Hutchison <luke.hutch@gmail.com>
2015-03-28 21:11:27 -0700

    Move charset before title

--- src/gribbit/response/HTMLPageResponse.java
+++ src/gribbit/response/HTMLPageResponse.java
@@ -105,6 +105,6 @@ public class HTMLPageResponse extends HTMLResponse {
      * element. The flashMessages parameter is filled in after the response is returned.
      */
     public static final String _template = "<!DOCTYPE html>" //
-            + "<html><head><title>${title}</title><meta charset=\"utf-8\"></head>" //
+            + "<html><head><meta charset=\"utf-8\"><title>${title}</title></head>" //
             + "<body>${flashMessages}${content}</body></html>";
 }
