5057be9c56e9a8ec817c95982c218a2f54b45785
Luke Hutchison <luke.hutch@gmail.com>
2015-06-17 03:32:28 -0700

    Refactoring

--- src/gribbit/util/RequestBuilder.java
+++ src/gribbit/util/RequestBuilder.java
@@ -25,13 +25,13 @@
  */
 package gribbit.util;
 
-import gribbit.response.exception.BadRequestException;
 import io.netty.handler.codec.http.HttpResponseStatus;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.StringWriter;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 import org.apache.commons.io.IOUtils;
@@ -39,59 +39,78 @@ import org.json.simple.JSONValue;
 
 public class RequestBuilder {
 
-    private static String getResponse(HttpURLConnection connection) throws IOException, IllegalArgumentException {
-        if (connection.getResponseCode() == HttpResponseStatus.OK.code()) {
-            StringWriter writer = new StringWriter();
-            IOUtils.copy(connection.getInputStream(), writer, "UTF-8");
-            return writer.toString();
-        } else {
-            throw new IllegalArgumentException("Got non-OK response code: " + connection.getResponseCode());
-        }
-    }
-
-    private static String makePOSTRequest(String url, String[] keyValuePairs) throws IllegalArgumentException {
-        HttpURLConnection connection = null;
-        try {
-            connection = (HttpURLConnection) new URL(url).openConnection();
-            connection.setDoOutput(true);
-            connection.setDoInput(true);
-            connection.setInstanceFollowRedirects(false);
-            connection.setRequestMethod("POST");
+    /** Make a connection to a given URL, and initiate a GET or POST request, returning the connection. */
+    private static HttpURLConnection makeConnection(String url, String[] keyValuePairs, boolean isGET)
+            throws MalformedURLException, IOException {
+        String reqURL = isGET ? url + "?" + WebUtils.buildQueryString(keyValuePairs) : url;
+        HttpURLConnection connection = (HttpURLConnection) new URL(reqURL).openConnection();
+        connection.setDoOutput(true);
+        connection.setDoInput(true);
+        connection.setInstanceFollowRedirects(false);
+        connection.setRequestMethod(isGET ? "GET" : "POST");
+        connection.setUseCaches(false);
+        if (!isGET) {
             connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
             connection.setRequestProperty("charset", "utf-8");
-            connection.setUseCaches(false);
             String params = WebUtils.buildQueryString(keyValuePairs);
             connection.setRequestProperty("Content-Length", Integer.toString(params.length()));
             try (DataOutputStream w = new DataOutputStream(connection.getOutputStream())) {
                 w.writeBytes(params);
                 w.flush();
             }
-            return getResponse(connection);
+        }
+        return connection;
+    }
+
+    private static String makeRequest(String url, String[] keyValuePairs, boolean isGET) {
+        HttpURLConnection connection = null;
+        try {
+            connection = makeConnection(url, keyValuePairs, isGET);
+            if (connection.getResponseCode() == HttpResponseStatus.OK.code()) {
+                StringWriter writer = new StringWriter();
+                IOUtils.copy(connection.getInputStream(), writer, "UTF-8");
+                return writer.toString();
+            } else {
+                throw new IllegalArgumentException("Got non-OK HTTP response code: " + connection.getResponseCode());
+            }
         } catch (Exception e) {
-            throw new IllegalArgumentException("Exception during POST request: " + e.getMessage(), e);
+            throw new IllegalArgumentException("Exception during " + (isGET ? "GET" : "POST") + " request: "
+                    + e.getMessage(), e);
         } finally {
             if (connection != null) {
-                connection.disconnect();
+                try {
+                    connection.disconnect();
+                } catch (Exception e) {
+                }
             }
         }
     }
 
-    private static String makeGETRequest(String url, String[] keyValuePairs) throws IllegalArgumentException {
+    /**
+     * Send a GET request to a given URL with the given key-value URL parameters, expecting a 302 Found redirect
+     * response. Returns the destination URL for the redirect.
+     * 
+     * @throws IllegalArgumentException
+     *             if request could not be completed or the response code was not 302 Found.
+     */
+    public static String getRedirectDestination(String url, String... keyValuePairs) throws IllegalArgumentException {
         HttpURLConnection connection = null;
         try {
-            String urlWithParams = url + "?" + WebUtils.buildQueryString(keyValuePairs);
-            connection = (HttpURLConnection) new URL(urlWithParams).openConnection();
-            connection.setDoOutput(true);
-            connection.setDoInput(true);
-            connection.setInstanceFollowRedirects(false);
-            connection.setRequestMethod("GET");
-            connection.setUseCaches(false);
-            return getResponse(connection);
+            connection = makeConnection(url, keyValuePairs, /* isGET = */true);
+            if (connection.getResponseCode() == HttpResponseStatus.FOUND.code()) {
+                return connection.getHeaderField("Location");
+            } else {
+                throw new IllegalArgumentException("Got non-FOUND HTTP response code: " + connection.getResponseCode());
+            }
         } catch (Exception e) {
-            throw new IllegalArgumentException("Exception during GET request: " + e.getMessage(), e);
+            throw new IllegalArgumentException("Exception while trying to determine redirect destination: "
+                    + e.getMessage(), e);
         } finally {
             if (connection != null) {
-                connection.disconnect();
+                try {
+                    connection.disconnect();
+                } catch (Exception e) {
+                }
             }
         }
     }
@@ -103,10 +122,10 @@ public class RequestBuilder {
      * @throws IllegalArgumentException
      *             if request could not be completed or JSON could not be mapped to the response type.
      */
-    public static <T> T postToURLAndBindJSONResponse(Class<T> responseType, String url, String... keyValuePairs)
+    public static <T> T postToURLWithJSONResponse(Class<T> responseType, String url, String... keyValuePairs)
             throws IllegalArgumentException {
+        String jsonStr = makeRequest(url, keyValuePairs, /* isGET = */false);
         try {
-            String jsonStr = makePOSTRequest(url, keyValuePairs);
             return JSONJackson.jsonToObject(jsonStr, responseType);
         } catch (Exception e) {
             throw new IllegalArgumentException("Could not parse JSON response: " + e.getMessage(), e);
@@ -122,8 +141,8 @@ public class RequestBuilder {
      */
     public static <T> T getFromURLWithJSONResponse(Class<T> responseType, String url, String... keyValuePairs)
             throws IllegalArgumentException {
+        String jsonStr = makeRequest(url, keyValuePairs, /* isGET = */true);
         try {
-            String jsonStr = makeGETRequest(url, keyValuePairs);
             return JSONJackson.jsonToObject(jsonStr, responseType);
         } catch (Exception e) {
             throw new IllegalArgumentException("Could not parse JSON response: " + e.getMessage(), e);
@@ -137,9 +156,9 @@ public class RequestBuilder {
      * @throws IllegalArgumentException
      *             if request could not be completed or JSON could not be parsed.
      */
-    public static Object postToURLWithJSONResponse(String url, String... keyValuePairs) throws BadRequestException {
+    public static Object postToURLWithJSONResponse(String url, String... keyValuePairs) throws IllegalArgumentException {
+        String jsonStr = makeRequest(url, keyValuePairs, /* isGET = */false);
         try {
-            String jsonStr = makePOSTRequest(url, keyValuePairs);
             return JSONValue.parse(jsonStr);
         } catch (Exception e) {
             throw new IllegalArgumentException("Could not parse JSON response: " + e.getMessage(), e);
@@ -153,9 +172,10 @@ public class RequestBuilder {
      * @throws IllegalArgumentException
      *             if request could not be completed or JSON could not be parsed.
      */
-    public static Object getFromURLWithJSONResponse(String url, String... keyValuePairs) throws BadRequestException {
+    public static Object getFromURLWithJSONResponse(String url, String... keyValuePairs)
+            throws IllegalArgumentException {
+        String jsonStr = makeRequest(url, keyValuePairs, /* isGET = */true);
         try {
-            String jsonStr = makeGETRequest(url, keyValuePairs);
             return JSONValue.parse(jsonStr);
         } catch (Exception e) {
             throw new IllegalArgumentException("Could not parse JSON response: " + e.getMessage(), e);
