713b01bab80fcfd9f92b43ea08154fefe75f6969
Luke Hutchison <luke.hutch@gmail.com>
2015-12-01 01:35:08 -0800

    Remove http package, it has been split out into separate project

--- src/gribbit/http/request/handler/HttpErrorHandler.java
+++
@@ -1,9 +0,0 @@
-package gribbit.http.request.handler;
-
-import gribbit.http.request.Request;
-import gribbit.http.response.GeneralResponse;
-import gribbit.http.response.exception.ResponseException;
-
-public interface HttpErrorHandler<E extends ResponseException> {
-    public GeneralResponse generateResponse(Request request, E e);
-}
--- src/gribbit/http/request/route/RouteMatcher.java
+++
@@ -1,9 +0,0 @@
-package gribbit.http.request.route;
-
-import gribbit.http.request.Request;
-import gribbit.http.response.GeneralResponse;
-import gribbit.http.response.exception.ResponseException;
-
-public abstract class RouteMatcher {
-    public abstract GeneralResponse match(Request request) throws ResponseException;
-}
